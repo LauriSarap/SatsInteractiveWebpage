ff89968cf6ad4064320ef11e96a1125714f7bff2
Lauri Sarap <lauri.aardla@gmail.com>
2024-01-13 01:12:27 +0200

    Initial project setup

---
+++ src/pages/TeamPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TeamPage = () => {
+    return (
+        <div>
+            <h2>About Our Team</h2>
+            {/* Add team information here */}
+        </div>
+    );
+};
+
+export default TeamPage;
